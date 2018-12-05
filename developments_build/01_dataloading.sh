c7c347d3013f2fd4897c79b0790c14c7a809eb8a
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-12-05 17:51:25 -0500

    starting structure for refactored developments db

---
+++ developments_build/01_dataloading.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+################################################################################################
+### OBTAINING DATA
+################################################################################################
+### NOTE: This script requires that you setup the DATABASE_URL environment variable.
+### Directions are in the README.md.
+
+## Load all datasets from sources using data-loading-scripts
+## https://github.com/NYCPlanning/data-loading-scripts
+
+## Open_datasets - PULLING FROM OPEN DATA
+echo 'Loading open source datasets...'
+node loader.js install dob_jobapplications
+node loader.js install dob_permitissuance
+
+node loader.js install dcp_cdboundaries
+node loader.js install dcp_ntaboundaries
+node loader.js install dcp_censusblocks
+node loader.js install dcp_school_districts
+
+
+## Other_datasets - PULLING FROM GitHub repo
+echo 'Loading datasets from GitHub repo...'
+node loader.js install dob_cofos
+node loader.js install housing_input_lookup_occupancy
+node loader.js install housing_input_lookup_status
+node loader.js install housing_input_dcpattributes
