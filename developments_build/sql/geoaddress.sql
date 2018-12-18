2b624a4508cdf18947cfa498fa1bcf3afb9c4faf
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-12-18 13:31:23 -0500

    fixing errors

---
+++ developments_build/sql/geoaddress.sql
@@ -0,0 +1,3 @@
+UPDATE developments a
+SET geo_address = trim(regexp_replace(a.geo_address_house, '\s+', ' ', 'g'))||' '||trim(regexp_replace(a.geo_address_street, '\s+', ' ', 'g'))
+WHERE a.geo_address_street IS NOT NULL;
